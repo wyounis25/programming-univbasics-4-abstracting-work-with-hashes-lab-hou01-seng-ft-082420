401df0cef5c0f68bdda52106acd16ef863e7a7cd
walid younis <walid.younis.90@gmail.com>
2020-08-18 03:41:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,9 +3,9 @@ def my_hash_creator(key, value)
   { key => value }
 end
 
-# def read_from_hash(hash, key)
-#   hash[key]
-# end
+def read_from_hash(hash, key)
+  hash[key]
+end
 
 # def update_counting_hash(hash, key)
 #   if hash[key]
